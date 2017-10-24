be6e56ee020a065846f16631a5911c122422d122
justpwned <justpwnedfrom@gmail.com>
2017-10-24 23:00:47 +0500

    Initial Commit

---
+++ strings.py
@@ -0,0 +1,37 @@
+game_over  = r"""
+ ____    __            ____
+|       /  \  | \ / | |
+|  __  |____| |  %  | |---
+|___|  |    | |     | |____
+
+  ___          ____   __
+ /   \ |    | |      |  \   
+ !   ! |    | |---   |__/
+ \___/  \__/  |____  | \
+
+
+     YOU FUCKED UP
+
+PRESS SPACE TO TRY AGAIN
+
+   PRESS ESCAPE TO QUIT
+
+"""
+
+win = """
+        CONGRATULATIONS MAN!    
+
+
+       You have just finished
+
+          "The Snake Game"   
+
+      by someone who you know 
+    
+    
+  Press Space to start a new game
+
+          Press Esc to quit
+
+           (HELLO ARTEM %)
+"""
